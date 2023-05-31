8c34b85ed083020b5d102906094d13b05e4a95bd
alirexaf <mr.afxali@gmail.com>
2023-05-31 09:58:19 +0330

    Factories

---
+++ TravelManagement.Domain/Factories/ITravelerChecklistFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelManagement.Domain.Consts;
+using TravelManagement.Domain.Entities;
+using TravelManagement.Domain.ValueObjects;
+
+namespace TravelManagement.Domain.Factories
+{
+    public interface ITravelerChecklistFactory
+    {
+        TravelerCheckList Create(TravelerCheckListId id, TravelerCheckListName name, TravelerCheckListDestination destination);
+        TravelerCheckList CreateWithDefaultItems(TravelerCheckListId id, TravelerCheckListName name, TravelDays days, 
+            Consts.Gender gender, Temperature temperature, TravelerCheckListDestination destination);
+    }
+}
---
+++ TravelManagement.Domain/Factories/TravelerChecklistFactory.cs
@@ -0,0 +1,27 @@
+﻿using TravelManagement.Domain.Entities;
+using TravelManagement.Domain.Policies;
+using TravelManagement.Domain.ValueObjects;
+
+namespace TravelManagement.Domain.Factories
+{
+    public sealed class TravelerChecklistFactory : ITravelerChecklistFactory
+    {
+        private readonly IEnumerable<ITravelerItemsPolicy> _policies;
+        public TravelerChecklistFactory(IEnumerable<ITravelerItemsPolicy> policies)
+            => _policies = policies;
+        public TravelerCheckList Create(TravelerCheckListId id, TravelerCheckListName name, 
+            TravelerCheckListDestination destination) => new(id, name, destination);
+        public TravelerCheckList CreateWithDefaultItems(TravelerCheckListId id, TravelerCheckListName name, TravelDays days,
+            Consts.Gender gender, Temperature temperature, TravelerCheckListDestination destination)
+        {
+            var data = new PolicyData(days, gender, temperature, destination);
+            var applicablePolicies = _policies.Where(p => p.IsApplicable(data));
+            
+            var items =  applicablePolicies.SelectMany(p => p.GenerateItems(data));
+            var travelerChecklist = Create(id, name, destination);
+
+            travelerChecklist.AddItems(items);
+            return travelerChecklist;
+        }
+    }
+}
